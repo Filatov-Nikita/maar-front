4e7961b1c47444ef8dcd0ae3ae93aebf3f1a16f2
Nikita Antonov <nikita45454@gmail.com>
2025-11-27 00:27:22 +0300

    style: добавляет начальные стили

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+  import 'swiper/css';
+</script>
+
+<style lang="scss">
+  @use '~/assets/scss/app.scss';
+</style>
