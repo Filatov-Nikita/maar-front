27b3692587e011d2439da3a7bf0d66d5ee839ac2
Nikita Antonov <nikita45454@gmail.com>
2025-11-27 00:33:08 +0300

    feat: home - добавляет секцию services

---
+++ app/components/Home/Services.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <BaseHeaderSec class="title" size="sm" design="secondary">Услуги</BaseHeaderSec>
+    <div class="items">
+      <ServicesListItem
+        class="list-item"
+        name="Выбрать<br>строящийся дом"
+        linkText="Предложим наши<br>объекты"
+        href="#"
+      />
+      <ServicesListItem
+        class="list-item"
+        name="Заказать подрядное<br>строительство"
+        linkText="Построим дом<br>по вашему проекту"
+        href="#"
+      />
+      <ServicesListItem
+        class="list-item"
+        name="Заказать дизайн<br>интерьера"
+        linkText="Сделаем проект<br>в вашем бюджете"
+        href="#"
+      />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+  .title {
+    margin-bottom: 18px;
+  }
+
+  .items {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .list-item {
+    width: 100%;
+
+    &:first-child {
+      border-top-width: 1px;
+    }
+  }
+</style>
---
+++ app/components/Services/ListItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <component class="item" :is="tag">
+    <div class="name" v-html="name"></div>
+    <div class="link-wrap">
+      <span class="link-text" v-html="linkText"></span>
+      <span class="icon">
+        <BaseIcon name="arrow-right-middle" fit />
+      </span>
+    </div>
+  </component>
+</template>
+
+<script setup lang="ts">
+  withDefaults(
+    defineProps<{
+      tag?: string,
+      name: string,
+      linkText: string,
+    }>(),
+    { tag: 'a' },
+  );
+</script>
+
+<style scoped lang="scss">
+  .item {
+    padding-top: 16px;
+    padding-bottom: 12px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    min-height: 200px;
+    border: 0 solid var(--color-black-1-2);
+    border-bottom-width: 1px;
+    overflow: hidden;
+
+    &:hover {
+      opacity: 0.7;
+
+      .icon {
+        transform: translateX(50px);
+      }
+    }
+  }
+
+  .name {
+    font-size: 25px;
+    line-height: 1;
+    letter-spacing: -0.04em;
+  }
+
+  .icon {
+    transition: transform 500ms;
+    width: 15px;
+    height: 12px;
+  }
+
+  .link-wrap {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    font-size: 15px;
+    line-height: 1.2;
+    letter-spacing: -0.03em;
+  }
+
+  .link-text {
+    flex-grow: 1;
+  }
+</style>
