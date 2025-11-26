4a0631d5837639da4d2b04345ab52fdfbd0885f4
Nikita Antonov <nikita45454@gmail.com>
2025-11-27 00:33:34 +0300

    feat: home - добавляет секцию commerc-text

---
+++ app/components/Home/CommercText.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <div class="photo-wrap">
+      <img class="w-full" width="738" height="880" src="~/assets/images/home/example2.jpg" alt="Пример интерьера" loading="lazy">
+    </div>
+    <BasePersonaText>
+      <p>
+        Maar Development — это лучший сервис, высокое качество строительства и&nbsp;превосходные локации.
+      </p>
+    </BasePersonaText>
+    <BasePersonaProfile
+      class="dir-profile"
+      :photoUrl="PhotoCommerc"
+      name="Коммерческий директор"
+      position="Эльза Шагабутдинова"
+    />
+  </section>
+</template>
+
+<script setup lang="ts">
+  import PhotoCommerc from '@/assets/images/personas/commerc.jpg';
+</script>
+
+<style scoped lang="scss">
+  .photo-wrap {
+    margin-bottom: 50px;
+  }
+
+  .dir-profile {
+    margin-top: 32px;
+    padding-bottom: 12px;
+    border-bottom: 1px solid var(--color-dark-1-2);
+  }
+</style>
