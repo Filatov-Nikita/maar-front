a844607db27b25f087c956770150cf93e3af457a
Nikita Antonov <nikita45454@gmail.com>
2025-11-27 00:26:10 +0300

    feat: добавляет плагин иконок

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import path from 'path';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  vite: {
+    plugins: [
+      createSvgIconsPlugin({
+        iconDirs: [ path.resolve(process.cwd(), 'app/assets/icons') ],
+        symbolId: 'icon-[dir]-[name]',
+        inject: 'body-last',
+        customDomId: '__svg__icons__dom__',
+      }),
+    ],
+  },
 })
