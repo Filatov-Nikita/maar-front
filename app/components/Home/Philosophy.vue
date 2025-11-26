e96b6cd2fa8844f5ff56d2a60cd6c786a23fb27b
Nikita Antonov <nikita45454@gmail.com>
2025-11-27 00:34:06 +0300

    feat: home - добавляет секцию philosophy

---
+++ app/components/Home/Philosophy.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <BaseHeaderSec class="title" design="secondary">Философия</BaseHeaderSec>
+    <p class="text1">
+      Мы создаём уникальные пространства для жизни,где в центре — человек,его состояние и ощущение. Мы вкладываем душу в каждый элемент и продумываем всё до мелочей
+    </p>
+    <p class="text2">
+      Нам важно, как человек будет чувствовать себя в построенном нами доме. Наши продукты отличаются максимальной комплектацией и зачастую превосходят продукты конкурентов.
+    </p>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+  .title {
+    margin-bottom: 125px;
+    padding-top: 12px;
+    border-top: 1px solid var(--color-dark-1-2);
+  }
+
+  .text1 {
+    font-size: 25px;
+    line-height: 1;
+    letter-spacing: -0.04em;
+    margin-bottom: 20px;
+    text-align: right;
+  }
+
+  .text2 {
+    font-size: 15px;
+    line-height: 1.2;
+    letter-spacing: -0.03em;
+    max-width: 272px;
+  }
+</style>
