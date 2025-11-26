c8fd2527717edc1901ff844b55d10e6e7f2eb379
Nikita Antonov <nikita45454@gmail.com>
2025-11-27 00:30:37 +0300

    feat: home - добавляет секцию main

---
+++ app/components/Home/Main.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="main">
+    <h1 class="h1 title">
+      Строим элитные резиденции в&nbsp;лучших локациях Москвы и&nbsp;области
+    </h1>
+    <div class="links">
+      <div class="link-item">
+        <BaseLinkPrimary href="/">
+          Riita
+        </BaseLinkPrimary>
+      </div>
+      <div class="link-item">
+        <BaseLinkPrimary href="/">
+          Agalarov Estate
+        </BaseLinkPrimary>
+      </div>
+      <div class="link-item">
+        <BaseLinkPrimary href="/">
+          Gorki
+        </BaseLinkPrimary>
+      </div>
+      <div class="link-item">
+        <BaseLinkPrimary href="/">
+          Millenium Park
+        </BaseLinkPrimary>
+      </div>
+    </div>
+    <div class="poster-block">
+      <img class="w-full" width="736" height="876" src="~/assets/images/temp/main-video.jpg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+  .main {
+    padding: 60px 0 30px;
+  }
+
+  .title {
+    max-width: 500px;
+    margin-bottom: 30px;
+  }
+
+  .poster-block {
+    margin-top: 70px;
+  }
+
+  .links {
+    display: flex;
+    flex-wrap: wrap;
+    margin: -5px;
+  }
+
+  .link-item {
+    width: 50%;
+    padding: 5px;
+  }
+</style>
