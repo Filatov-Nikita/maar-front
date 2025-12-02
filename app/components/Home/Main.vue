0b3ac54867ac1fd0bbe24be739517ece058c3798
Nikita Antonov <nikita45454@gmail.com>
2025-12-02 11:28:11 +0300

    style: home | main - добавляет адаптив

--- app/components/Home/Main.vue
+++ app/components/Home/Main.vue
@@ -1,62 +1,98 @@
 <template>
   <div class="main">
-    <h1 class="h1 title">
-      Строим элитные резиденции в&nbsp;лучших локациях Москвы и&nbsp;области
-    </h1>
-    <div class="links">
-      <div class="link-item">
-        <BaseLinkPrimary href="/">
-          Riita
-        </BaseLinkPrimary>
-      </div>
-      <div class="link-item">
-        <BaseLinkPrimary href="/">
-          Agalarov Estate
-        </BaseLinkPrimary>
-      </div>
-      <div class="link-item">
-        <BaseLinkPrimary href="/">
-          Gorki
-        </BaseLinkPrimary>
-      </div>
-      <div class="link-item">
-        <BaseLinkPrimary href="/">
-          Millenium Park
-        </BaseLinkPrimary>
+    <div class="wrap">
+      <h1 class="h1 title">
+        Строим элитные резиденции в&nbsp;лучших локациях Москвы и&nbsp;области
+      </h1>
+      <div class="links">
+        <div class="link-item">
+          <BaseLinkPrimary href="/">
+            Riita
+          </BaseLinkPrimary>
+        </div>
+        <div class="link-item">
+          <BaseLinkPrimary href="/">
+            Agalarov Estate
+          </BaseLinkPrimary>
+        </div>
+        <div class="link-item">
+          <BaseLinkPrimary href="/">
+            Gorki
+          </BaseLinkPrimary>
+        </div>
+        <div class="link-item">
+          <BaseLinkPrimary href="/">
+            Millenium Park
+          </BaseLinkPrimary>
+        </div>
       </div>
     </div>
     <div class="poster-block">
-      <img class="w-full" width="736" height="876" src="~/assets/images/temp/main-video.jpg" alt="" />
+      <BaseImageResp
+        pictute
+        class="w-full"
+        width="736"
+        height="876"
+        :url="ImageVideo"
+        :md="{
+          width: 2112,
+          height: 1042,
+          url: ImageVideoXl,
+        }"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
+  import ImageVideo from '~/assets/images/temp/main-video.jpg';
+  import ImageVideoXl from '~/assets/images/temp/main-video-xl.jpg';
 </script>
 
 <style scoped lang="scss">
   .main {
     padding: 60px 0 30px;
+
+    @include xl {
+      padding-bottom: 40px;
+      padding-top: 90px;
+    }
   }
 
   .title {
     max-width: 500px;
     margin-bottom: 30px;
+
+    @include xl {
+      margin-bottom: 0px;
+    }
   }
 
   .poster-block {
     margin-top: 70px;
+
+    @include xl {
+      margin-top: 30px;
+    }
+  }
+
+  .wrap {
+    @include xl {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      column-gap: 50px;
+      justify-content: space-between;
+    }
   }
 
   .links {
-    display: flex;
-    flex-wrap: wrap;
-    margin: -5px;
+    @include row();
+    @include gap(5px, 5px);
   }
 
   .link-item {
-    width: 50%;
-    padding: 5px;
+    @include col();
+    @include size(50%);
   }
 </style>
