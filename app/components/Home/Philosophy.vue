2319092f6bc3596c3f2ae3cfd074f4107d10a2f0
Nikita Antonov <nikita45454@gmail.com>
2025-12-02 11:31:37 +0300

    style: home | philosophy - добавляет адаптив

--- app/components/Home/Philosophy.vue
+++ app/components/Home/Philosophy.vue
@@ -1,17 +1,21 @@
 <template>
   <section>
-    <BaseHeaderSec class="title" design="secondary">Философия</BaseHeaderSec>
-    <p class="text1">
-      Мы создаём уникальные пространства для жизни,где в центре — человек,его состояние и ощущение. Мы вкладываем душу в каждый элемент и продумываем всё до мелочей
-    </p>
-    <p class="text2">
-      Нам важно, как человек будет чувствовать себя в построенном нами доме. Наши продукты отличаются максимальной комплектацией и зачастую превосходят продукты конкурентов.
-    </p>
+    <BaseHeaderSec class="title" design="secondary" :size="grid.lg ? 'lg' : 'sm'">
+      Философия
+    </BaseHeaderSec>
+    <div class="text-body">
+      <p class="text1">
+        Мы создаём уникальные пространства для&nbsp;жизни, где в&nbsp;центре — человек, его&nbsp;состояние и&nbsp;ощущение. Мы&nbsp;вкладываем душу в&nbsp;каждый элемент и&nbsp;продумываем всё до&nbsp;мелочей
+      </p>
+      <p class="text2">
+        Нам важно, как&nbsp;человек будет чувствовать себя в&nbsp;построенном нами доме. Наши продукты отличаются максимальной комплектацией и&nbsp;зачастую превосходят продукты конкурентов.
+      </p>
+    </div>
   </section>
 </template>
 
 <script setup lang="ts">
-
+  const grid = useAppGrid();
 </script>
 
 <style scoped lang="scss">
@@ -19,20 +23,47 @@
     margin-bottom: 125px;
     padding-top: 12px;
     border-top: 1px solid var(--color-dark-1-2);
+
+    @include lg {
+      padding-top: 24px;
+      margin-bottom: 180px;
+    }
+  }
+
+  .text-body {
+    @include lg {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      align-items: flex-end;
+      gap: 33px;
+    }
   }
 
   .text1 {
+    width: 100%;
     font-size: 25px;
     line-height: 1;
     letter-spacing: -0.04em;
     margin-bottom: 20px;
     text-align: right;
+
+    @include lg {
+      font-size: 30px;
+      max-width: 689px;
+      margin-bottom: 0px;
+    }
   }
 
   .text2 {
+    width: 100%;
     font-size: 15px;
     line-height: 1.2;
     letter-spacing: -0.03em;
     max-width: 272px;
+
+    @include lg {
+      max-width: 337px;
+    }
   }
 </style>
