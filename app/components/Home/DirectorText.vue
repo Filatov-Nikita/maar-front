60e7d4a89be0fa3e66f552959a8b4dc7c44ed8e5
Nikita Antonov <nikita45454@gmail.com>
2025-12-02 11:30:09 +0300

    style: home | director-text - добавляет адаптив

--- app/components/Home/DirectorText.vue
+++ app/components/Home/DirectorText.vue
@@ -1,34 +1,90 @@
 <template>
-  <section>
-    <div class="photo-wrap">
-      <img class="w-full" width="740" height="548" src="~/assets/images/home/example.jpg" alt="Пример интерьера" loading="lazy">
+  <section class="dir-text">
+    <div class="grid">
+      <div class="left">
+        <div class="photo-wrap">
+          <BaseImageResp
+            class="w-full"
+            width="740"
+            height="548"
+            :url="ImageExample"
+            alt="Пример интерьера"
+            loading="lazy"
+            pictute
+            :md="{
+              width: 1400,
+              height: 1041,
+              url: ImageExampleXl,
+            }"
+          />
+        </div>
+      </div>
+      <div class="right">
+        <BasePersonaText class="pers-text">
+          <p>
+            Воплощая в реальность самые смелые идеи и желания, мы органично соединяем технологические инженерные новшества, трендовые направления и материалы.
+          </p>
+        </BasePersonaText>
+        <BasePersonaProfile
+          class="dir-profile"
+          :photoUrl="PhotoDir"
+          name="Генеральный директор"
+          position="Насыров Руслан"
+        />
+      </div>
     </div>
-    <BasePersonaText>
-      <p>
-        Воплощая в реальность самые смелые идеи и желания, мы органично соединяем технологические инженерные новшества, трендовые направления и материалы.
-      </p>
-    </BasePersonaText>
-    <BasePersonaProfile
-      class="dir-profile"
-      :photoUrl="PhotoDir"
-      name="Генеральный директор"
-      position="Насыров Руслан"
-    />
   </section>
 </template>
 
 <script setup lang="ts">
   import PhotoDir from '@/assets/images/personas/dir.jpg';
+  import ImageExample from '@/assets/images/home/example.jpg';
+  import ImageExampleXl from '@/assets/images/home/example-xl.jpg';
 </script>
 
 <style scoped lang="scss">
-  .photo-wrap {
-    margin-bottom: 50px;
+  .grid {
+    @include gap(0px, 50px);
+    @include gap-xl(34px, 0px);
+    @include row();
+
+    @include xl {
+      align-items: flex-end;
+    }
+  }
+
+  .left {
+    @include col();
+    @include size(100%);
+    @include size-xl(68%);
+  }
+
+  .right {
+    @include col();
+    @include size(100%);
+    @include size-xl(32%);
   }
 
   .dir-profile {
     margin-top: 32px;
     padding-bottom: 12px;
     border-bottom: 1px solid var(--color-dark-1-2);
+
+    @include xl {
+      padding-bottom: 0;
+      border-bottom-width: 0;
+    }
+  }
+
+  .pers-text {
+    --offset: 75px;
+    font-size: 19px;
+    line-height: 1.2;
+    letter-spacing: -0.04em;
+
+    @include xl {
+      font-size: 23px;
+      --offset: 90px;
+    }
   }
 </style>
