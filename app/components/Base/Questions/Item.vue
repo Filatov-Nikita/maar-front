899d46203f39f623a0ce8c05db6708965f21a44b
Nikita Antonov <nikita45454@gmail.com>
2025-12-08 17:29:36 +0300

    feat: добавляет базовые компоненты вопросов и ответов

---
+++ app/components/Base/Questions/Item.vue
@@ -0,0 +1,71 @@
+<template>
+  <article class="item">
+    <div class="title-wrap" @click="showed = !showed">
+      <span class="icon">
+        <BaseIcon name="dash" fit />
+      </span>
+      <component class="title" :is="titleTag">
+        <slot class="title" name="title"></slot>
+      </component>
+    </div>
+    <SlideUpDown class="body-wrap" :active="showed" :duration="300">
+      <div class="text">
+        <slot name="text"></slot>
+      </div>
+    </SlideUpDown>
+  </article>
+</template>
+
+<script setup lang="ts">
+  //@ts-ignore
+  import SlideUpDown from 'vue-slide-up-down';
+
+  defineProps<{
+    titleTag: string,
+  }>();
+
+  const showed = ref(false);
+</script>
+
+<style scoped lang="scss">
+  .item {
+    padding: 20px 0 18px;
+    border-top: 1px solid var(--color-black-1-2);
+
+    &:last-child {
+      border-bottom: 1px solid var(--color-black-1-2);
+    }
+  }
+
+  .title-wrap {
+    display: flex;
+    gap: 16px;
+    align-items: flex-start;
+  }
+
+  .title {
+    font-size: 19px;
+    line-height: 1.25;
+    letter-spacing: -0.03em;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.5;
+    }
+  }
+
+  .text {
+    font-size: 15px;
+    line-height: 1.25;
+    letter-spacing: -0.03em;
+    padding-top: 18px;
+    padding-left: 41px;
+  }
+
+  .icon {
+    margin-top: 13px;
+    flex-shrink: 0;
+    width: 25px;
+    height: 2px;
+  }
+</style>
---
+++ app/components/Base/Questions/Section.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <BaseHeaderSec class="title" tag="h2" size="lg" design="secondary">
+      Часто задаваемые вопросы
+    </BaseHeaderSec>
+    <slot></slot>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+  .title {
+    margin-bottom: 24px;
+  }
+</style>
